9307833821421dbf203a8dfdf557c78f95b03f8c
ponmalar <coder@effddfdaeebcfacbdcbaeaefcbdabbdaddfbdb-0.effddfdaeebcfacbdcbaeaefcbdabbdaddfbdb.che.svc.cluster.local>
2023-01-25 08:19:56 +0000

    CEL02

---
+++ CEL02/reactapp/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';  
+class Clock extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {date: new Date()};
+   }
+ 
+   componentDidMount() {
+     this.timerID = setInterval(
+       () => this.tick(),
+       1000
+     );
+   }
+ 
+   componentWillUnmount() {
+     clearInterval(this.timerID);
+   }
+ 
+   tick() {
+     this.setState({
+       date: new Date()
+     });
+   }
+ 
+   render() {
+     return (
+       <div>
+         <h1>Now the Time :</h1>
+         <h2> {this.state.date.toLocaleTimeString()}.</h2>
+         <h3>Check Life Cycle of Clock Component</h3>
+       </div>
+     );
+   }
+ }
+
+function App() {
+   return (
+     <div>
+       <Clock />
+     </div>
+   );
+ }
+export default App;  
--- CEL02/reactapp/src/tests/App.test.js
+++ CEL02/reactapp/src/tests/App.test.js
@@ -3,11 +3,11 @@ import App from '../App';
 
 test('test_case01', () => {
   render(<App />);
-  const linkElement = screen.getByText(/IamNEO/i);
+  const linkElement = screen.getByText(/Now the Time/i);
   expect(linkElement).toBeInTheDocument();
-});
+})
 test('test_case02', () => {
   render(<App />);
-  const linkElement = screen.getByRole('button');
+  const linkElement = screen.getByText(/Check Life Cycle of Clock Component/i);
   expect(linkElement).toBeInTheDocument();
 });
