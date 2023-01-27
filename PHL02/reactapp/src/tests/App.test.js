f7af85532ef0773a7b57f5804410ccbaa21c38c9
Ponmalar <ponmalar@iamneo.ai>
2023-01-27 09:56:38 +0000

    PHL02

--- PHL02/reactapp/src/tests/App.test.js
+++ PHL02/reactapp/src/tests/App.test.js
@@ -3,17 +3,25 @@ import App from '../App';
 
 test('test_case01', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Enter Your Name/i);
+  const linkElement = screen.getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
 })
 
+
 test('test_case02', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Enter Your Age/i);
+  const linkElement = screen.getByText(/ age /i);
   expect(linkElement).toBeInTheDocument();
 })
+
 test('test_case03', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Greeting/i);
+  const linkElement = screen.getByRole('button');
+  expect(linkElement).toBeInTheDocument();
+})
+
+test('test_case04', () => {
+  render(<App />);
+  const linkElement = screen.getByRole('textbox');
   expect(linkElement).toBeInTheDocument();
 })
